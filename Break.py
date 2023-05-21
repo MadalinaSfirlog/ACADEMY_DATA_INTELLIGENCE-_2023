26722dc4e55abe28ff31676cdc98f74ff7059586
Madalina Sfirlog <126765250+MadalinaSfirlog@users.noreply.github.com>
2023-05-21 18:18:54 +0200

    Update Break.py

--- Break.py
+++ Break.py
@@ -7,4 +7,25 @@ for numero in lista_numeri:
         if numero == "0": 
             break
         else: 
-            print(numero)    
+            print(numero) 
+    
+
+
+
+ 
+#Creare un menu dove si da la possibilità all'utente di inserire una serie di numeri 
+#Se il numero è diverso da zero il menu deve mandare a schermo i numeri inseriti 
+#Se il numero è uguale a zero il ciclo deve interrompersi e l'accensione deve spegnersi
+
+accensione = True
+while accensione: 
+    lista_numeri = input("Inserisci i numeri: ").split()               
+            
+    for numero in lista_numeri:            
+     if numero != "0":  
+        print(numero)
+     else:
+        break
+        accensione=False
+
+
